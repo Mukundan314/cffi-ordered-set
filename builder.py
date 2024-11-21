fff876ee77ac15138a1cc8b2b9bba3b9ca72850b
Mukundan314 <mukundan314@gmail.com>
2024-11-21 13:17:43 +0530

    ???

--- builder.py
+++ builder.py
@@ -26,6 +26,7 @@ int T_ge(void *t_ptr, int x);
 ffibuilder.set_source(
     "_output",
     """
+extern "C" {
 void *T_new(int n, int *data);
 void T_insert(void *t_ptr, int x);
 void T_erase(void *t_ptr, int x);
@@ -33,6 +34,7 @@ int T_kth(void *t_ptr, int k);
 int T_rank(void *t_ptr, int x);
 int T_le(void *t_ptr, int x);
 int T_ge(void *t_ptr, int x);
+}
 """,
     sources=["main.cpp"],
     source_extension=".cpp",
